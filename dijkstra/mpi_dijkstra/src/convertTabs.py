c9578dac4e021d1780cdbd87041cc92bd56ffabd
Joseph Imseis <marchen144@aim.com>
2019-11-09 15:47:20 -0600

    Made bitcoin.txt using convertTabs.py.

--- dijkstra/mpi_dijkstra/src/convertTabs.py
+++ dijkstra/mpi_dijkstra/src/convertTabs.py
@@ -1,3 +1,3 @@
-with open('er.txt') as fin, open('enrol.txt', 'w') as fout:
+with open('tabs.txt') as fin, open('bitcoin.txt', 'w') as fout:
     for line in fin:
         fout.write(line.replace('\t', ' '))
